4ff5d41936e73b83b4828cde6c346d157dc25860
Alex Everett <aeverett4746@gmail.com>
2024-01-22 21:54:00 -0700

    Abandoned parse for now, focusing on Python scripts

---
+++ count_var_sites.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+
+import sys
+from Bio import SeqIO
+from Bio.Seq import Seq
+
+def read_sequences(file):
+    sequences = SeqIO.to_dict(SeqIO.parse(file, "fasta"))
+    return sequences
+
+def translate_to_aa(sequences):
+    translated_seq = {}
+    for seq_id, dna_sequence in sequences.items():
+        dna_seq = Seq(str(dna_sequence.seq))
+        amino_acid_seq = dna_seq.translate()
+        translated_seq[seq_id] = amino_acid_seq
+    return translated_seq
+
+def count_var_site(seq_objects):
+    count = 0
+    sequences = list(seq_objects.values())
+    for pos in range(len(sequences[0])):
+        letters = [seq[pos] for seq in sequences]
+        if len(set(letters)) > 1:
+            count += 1
+    return count
+
+
+def main():
+    alignment = sys.argv[1]
+    sequences = read_sequences(alignment)
+    translated_seqs = translate_to_aa(sequences)
+    var_site_n = count_var_site(sequences)
+    var_site_aa = count_var_site(translated_seqs)
+    print(var_site_n, var_site_aa)
+
+if __name__=="__main__":
+    main()
--- parse_alignment.sh
+++ parse_alignment.sh
@@ -3,10 +3,12 @@
 alignment="$1"
 
 total_samples=$(grep "^>" "$alignment" | wc -l)
-unique_dates=$(grep "^>Sample[0-9]+_[0-9]{1,2}+-[a-zA-Z]+" "$alignment" | sort | uniq)
-num_unique=$(echo "$unique_dates" | wc -l)
+
+#COME BACK TO REGULAR EXPRESSIONS
+#unique_dates=$(grep -o "^>Sample[0-9]+_[0-9]{1,2}+-[a-zA-Z]\+" "$alignment" | sort | uniq)
+#num_unique=$(echo "$unique_dates" | wc -l)
 
 echo "Total Samples: $total_samples" > log.txt
-echo "Unique Sample Dates: $num_unique" >> log.txt
+#echo "Unique Sample Dates: $num_unique" >> log.txt
 
 echo "Processing complete. Results written to log.txt"
