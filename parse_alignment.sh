359619e67bc120356f4cd0990a0690387d9447ff
Alex Everett <aeverett4746@gmail.com>
2024-01-23 14:37:00 -0700

    Finally figured out parsing script

--- parse_alignment.sh
+++ parse_alignment.sh
@@ -4,7 +4,7 @@ alignment="$1"
 
 total_samples=$(grep "^>" "$alignment" | wc -l)
 
-unique_dates=$(grep "[0-9]{1,2}-[a-zA-Z]{3}" "$alignment" | uniq | wc -l)
+unique_dates=$(grep -o "[0-9]*-[a-zA-Z]*" "$alignment" | uniq | wc -l)
 
 echo "Total Samples: $total_samples" > log.txt
 echo "Unique Sample Dates: $unique_dates" >> log.txt
